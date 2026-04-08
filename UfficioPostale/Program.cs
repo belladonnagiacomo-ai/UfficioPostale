67506790fdc27b2feb442fa458e78477310393f9
belladonnagiacomo <belladonna.giacomo@franchettisalviani.net>
2026-04-08 12:17:10 +0200

    Aggiungere i file di progetto.

---
+++ UfficioPostale/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UfficioPostale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
